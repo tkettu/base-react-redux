0c4fe572c72aa1cb3d9b575a7e9b1ccf4d6ab3a7
Tero Kettunen <terojkettunen@gmail.com>
2018-08-13 13:01:35 +0300

    mock server reorganizing

--- server/server.js
+++ server/server.js
@@ -1,40 +1,39 @@
-const fs = require('fs')
+//const fs = require('fs')
 const bodyParser = require('body-parser')
 const jsonServer = require('json-server')
-const jwt = require('jsonwebtoken')
+//const jwt = require('jsonwebtoken')
+
+const loginController = require('./controllers/login')
 
 const server = jsonServer.create()
 
 const router = jsonServer.router('db.json')
 
-const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
+//const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
 
 server.use(jsonServer.defaults())
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 
 
-const SECRET_KEY='V3R153K9E7'
-const expiresIn = '1h'
+// const SECRET_KEY='V3R153K9E7'
+// const expiresIn = '1h'
 
-const createToken = (payload) => {
-    return jwt.sign(payload, SECRET_KEY/*, {expiresIn} */  )
-}
+// const createToken = (payload) => {
+//     return jwt.sign(payload, SECRET_KEY/*, {expiresIn} */  )
+// }
 
-const verifyToken = (token) => {
-    return  jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ?  decode : err)
-}
+// const verifyToken = (token) => {
+//     return  jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ?  decode : err)
+// }
 
-const isAuthenticated = ({email, password}) => {
-    return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
-}
+// const isAuthenticated = ({email, password}) => {
+//     return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
+// }
 
 server.post('/api/login', (req, res) => {
-    console.log('POST LOGIN')
-    //console.log(res)
-    //console.log(req.headers)
-    console.log(req.body)
-    const {username, password} = req.body
+    loginController.loginPost(req, res)
+    /* const {username, password} = req.body
     console.log('POSTATAAN  LOGIN PYYNTÖ', username, password)
     if (!isAuthenticated({username, password})){
         const status = 401
@@ -44,13 +43,12 @@ server.post('/api/login', (req, res) => {
         return
     }
     const access_token = createToken({username, password})
-    console.log(`accessi tokeni ${access_token}`)
-    res.status(200).json({access_token, username})
+    res.status(200).json({access_token, username}) */
 })
 
 server.use((req,res,next) => {
-    console.log('SERVERI USE')
-    if (req.headers.authorization === undefined 
+    loginController.loginUse(req, res, next)
+    /* if (req.headers.authorization === undefined 
         || req.headers.authorization.split(' ')[0] 
         !== 'Bearer'){
             const status = 401
@@ -65,33 +63,8 @@ server.use((req,res,next) => {
             const status = 401
             const message = 'Error: access_token is not valid'
             res.status(status).json({status, message})
-        }
+        } */
 })
-/*
-server.post('/api/login', (req, res) => {
-    console.log('POST login')
-    const status = 401
-    const msg = 'BAD DOG'
-    res.status(status).json({status, msg})
-})
-
-const isAuthorized = (req) => {
-    console.log(req)
-    return true
-}
-
-server.use((req, res, next) => {
-    if(isAuthorized(req)){
-        console.log('AUHTENTIKOIDAAN')
-        next()
-    }else {
-        res.sendStatus(401)
-    }
-
-})*/
-
-
-//https://www.techiediaries.com/fake-api-jwt-json-server/
 
 const PORT = 3001
 server.use('/api', router)
