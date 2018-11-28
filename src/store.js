9766377c18f9e1ba26fc0d428e648ff523542572
Tero Kettunen <terojkettunen@gmail.com>
2018-11-28 15:14:00 +0200

    updated dependencies

--- src/store.js
+++ src/store.js
@@ -11,7 +11,7 @@ const reducer = combineReducers({
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(thunk)
 )
 
